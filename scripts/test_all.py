d742b4e932aaaab9fad71a94ae5f4b9e12ba3586
xieit <914117787@qq.com>
2018-03-31 15:22:41 +0800

    第二次修改后提交

--- scripts/test_all.py
+++ scripts/test_all.py
@@ -4,9 +4,9 @@ class Test_allure:
     def test_al(self):
         assert 1
 
-    def test_al(self):
+    def test_al01(self):
         assert 0
 
-    def test_al(self):
+    def test_al02(self):
         assert 1
 
